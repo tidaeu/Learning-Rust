b7c638ee54937ea368ef49a8eb63b3647671edfc
Tim <tdeuken@gmail.com>
2026-07-06 11:08:18 -1000

    small commit

---
+++ hello_cargo/target/debug/deps/hello_cargo-3785c3006f7f892f.d
@@ -0,0 +1,5 @@
+C:\Users\tdeuk\Gitland\Rust\hello_cargo\target\debug\deps\hello_cargo-3785c3006f7f892f.d: src\main.rs
+
+C:\Users\tdeuk\Gitland\Rust\hello_cargo\target\debug\deps\libhello_cargo-3785c3006f7f892f.rmeta: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/deps/hello_cargo-475289df446be880.d
@@ -0,0 +1,5 @@
+C:\Users\tdeuk\Gitland\Rust\hello_cargo\target\debug\deps\hello_cargo-475289df446be880.d: src\main.rs
+
+C:\Users\tdeuk\Gitland\Rust\hello_cargo\target\debug\deps\libhello_cargo-475289df446be880.rmeta: src\main.rs
+
+src\main.rs:
